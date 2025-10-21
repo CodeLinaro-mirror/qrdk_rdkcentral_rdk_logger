804531a3ad6e2cae4cb14aa90a4c35285a305dd7
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2025-10-21 10:27:01 +0530

    test gdl socket variable value

--- unittests/rdkloggerInit.cpp
+++ unittests/rdkloggerInit.cpp
@@ -73,8 +73,8 @@ TEST(rdkloggerInit, rdkLoggerInit_check_enabled)
   ret = rdk_logger_is_logLevel_enabled( "LOG.RDK.ONLYERROR", RDK_LOG_ERROR);
   EXPECT_EQ(ret, TRUE)<<"rdk_logger_is_logLevel_enabled for RDK_LOG_ERROR failed with return:\""<<ret<<"\"";
 
-  ret = rdk_logger_deinit();
-  ASSERT_EQ(ret,RDK_SUCCESS)<<"rdk_logger_deinit failed with return:"<<ret;
+  //ret = rdk_logger_deinit();
+  //ASSERT_EQ(ret,RDK_SUCCESS)<<"rdk_logger_deinit failed with return:"<<ret;
 }
 
 TEST(rdkloggerInit, rdkLogger_ReInit_check_enabled)
@@ -90,6 +90,9 @@ TEST(rdkloggerInit, rdkLogger_ReInit_check_enabled)
   ASSERT_EQ(ret,RDK_SUCCESS)<<"rdk_logger_init failed:"<<ret<<", could not open user configuration file:"<<conf_file;
   ret = rdk_logger_is_logLevel_enabled( "LOG.RDK.ONLYERROR", RDK_LOG_ERROR);
   EXPECT_EQ(ret,TRUE)<<"rdk_logger_is_logLevel_enabled for RDK_LOG_ERROR failed with return:\""<<ret<<"\"";
+
+ // ret = rdk_logger_deinit();
+ // ASSERT_EQ(ret,RDK_SUCCESS)<<"rdk_logger_deinit failed with return:"<<ret;
 
 }
 
