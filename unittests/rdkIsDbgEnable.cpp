2b81658b1c177d94f17f9f3799e9fc6c198b028b
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2025-10-21 15:22:58 +0530

    test gdl socket variable value

--- unittests/rdkIsDbgEnable.cpp
+++ unittests/rdkIsDbgEnable.cpp
@@ -40,6 +40,21 @@ TEST(rdkIsDbgEnable, NULL_log_enabled)
   //ASSERT_EQ(ret,RDK_SUCCESS)<<"rdk_logger_deinit failed with return:\""<<ret<<"\" "<<"DEBUG_CONF_FILE is not found";
 }
 
+TEST(rdkIsDbgEnable, NULL_logname_log_enabled)
+{
+
+  rdk_logger_Bool ret =  TRUE;
+  rdk_Error rdk_err = RDK_SUCCESS;
+  char conf_file[] = GTEST_DEBUG_INI_FILE;
+
+  rdk_err = rdk_logger_init(conf_file);
+  ASSERT_EQ(rdk_err,RDK_SUCCESS)<<"rdk_logger_init failed with err:\""<<rdk_err<<"\", Could not open user configuration file:"<<conf_file;
+  ret = rdk_logger_enable_logLevel(NULL, 8, TRUE);
+  EXPECT_EQ(ret, FALSE)<<"rdk_logger_enabled_loglevel failed with return:\""<<ret<<"\"";
+  //ret = rdk_logger_deinit();
+  //ASSERT_EQ(ret,RDK_SUCCESS)<<"rdk_logger_deinit failed with return:\""<<ret<<"\" "<<"DEBUG_CONF_FILE is not found";
+}
+
 TEST(rdkIsDbgEnable, ONLYFATAL_log_enabled)
 {
 
