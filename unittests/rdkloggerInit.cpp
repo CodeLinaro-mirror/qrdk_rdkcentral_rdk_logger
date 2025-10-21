f9df87492d0a293dce4982fd6be77dd3e1b9c6cd
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2025-10-21 18:14:35 +0530

    test gdl socket variable value

--- unittests/rdkloggerInit.cpp
+++ unittests/rdkloggerInit.cpp
@@ -28,12 +28,12 @@ Test Case : Testing rdk_logger function rdk_logger_msg_printf
 /*DIABLED some test cases as there's a bug "https://bugzilla.redhat.com/show_bug.cgi?id=1901955" in log4c library, calling log4c_init twice causing segmentation fault, hence keeping rdk_logger_deinit()'s default changes which does not allow calling rdk_logger_init twice*/
 
 
-TEST(rdkloggerInit, DISABLED_rdkLoggerInit_with_DebugConfFile)
+TEST(rdkloggerInit, rdkLoggerInit_with_DebugConfFile)
 {
-  rdk_Error ret = RDK_SUCCESS;
+  rdk_Error ret = -1;
   char *conf_file = NULL;
   ret = rdk_logger_init(conf_file);
-  ASSERT_EQ(ret,RDK_SUCCESS)<<"rdk_logger_init failed with return:\""<<ret<<"\" "<<"config_file ini is not found";
+  ASSERT_EQ(ret,-1)<<"rdk_logger_init failed with return:\""<<ret<<"\" "<<"config_file ini is not found";
 }
 
 TEST(rdkloggerInit, DISABLED_rdkLoggerInitDeinit_with_DebugConfFile)
