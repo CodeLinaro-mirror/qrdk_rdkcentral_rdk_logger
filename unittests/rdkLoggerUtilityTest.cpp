40e4772e1596885c944c5932ab652a49e4732285
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2025-10-23 16:44:40 +0530

    test

--- unittests/rdkLoggerUtilityTest.cpp
+++ unittests/rdkLoggerUtilityTest.cpp
@@ -16,12 +16,11 @@ TEST(RDKLoggerUtilityTest, LogOnboardFunctionality) {
     char conf_file[] = GTEST_DEBUG_INI_FILE;	
     rdk_Error ret = rdk_logger_init(conf_file);
     ASSERT_EQ(ret, RDK_SUCCESS) << "Failed to initialize logger";
-    getUptimeMS();
     // Test onboard logging
     rdk_logger_log_onboard("LOG.RDK.ONBOARD", "Test onboard message");
     rdk_logger_log_onboard("LOG.RDK.ONBOARD", "Onboard message with format: %d", 123);
     rdk_logger_log_onboard("LOG.RDK.ONBOARD", "Onboard message with multiple args: %s %d %f", "test", 456, 3.14);
-
+    logMilestone("APPLICATION READY");
     // Should work correctly
 }
 
