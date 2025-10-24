a9710ee8014285a1dec0ae3f968b9eccca753a7a
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2025-10-24 14:54:54 +0530

    test

--- unittests/rdkLoggerRotationTest.cpp
+++ unittests/rdkLoggerRotationTest.cpp
@@ -241,7 +241,7 @@ TEST_F(RDKLoggerRotationTest, CountBasedRotation) {
     char large_message[100];
     createLargeLogMessage(large_message, sizeof(large_message));
     
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 2; i++) {
         rdk_logger_msg_printf(RDK_LOG_WARN, "LOG.RDK.ROTATION", "Message %d: %s", i, large_message);
     }
     
