3738f4f8a2ab7b7bbfea3db86ac729fdcb5382cf
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2025-10-24 17:04:00 +0530

    test

--- unittests/rdkLoggerErrorTest.cpp
+++ unittests/rdkLoggerErrorTest.cpp
@@ -66,10 +66,10 @@ TEST_F(RDKLoggerErrorTest, NullPointerHandling) {
     // Should not crash, but may not log anything
     
     // Test NULL format string
-    rdk_logger_msg_printf(RDK_LOG_WARN, "LOG.RDK.TEST", NULL);
+//    rdk_logger_msg_printf(RDK_LOG_WARN, "LOG.RDK.TEST", NULL);
     // Should not crash
     
-#if 0 
+#if 1 
     // Test NULL module name in enable_logLevel
     rdk_logger_Bool result = rdk_logger_enable_logLevel(NULL, RDK_LOG_WARN, TRUE);
     EXPECT_EQ(result, FALSE) << "Should return FALSE for NULL module name";
