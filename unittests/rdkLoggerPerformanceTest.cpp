5b5f1483976532d6dec92f6afa5d86803c09c13d
Deepthi C Shetty <115452109+dshett549@users.noreply.github.com>
2025-11-03 13:57:23 +0530

    Update rdkLoggerPerformanceTest.cpp

--- unittests/rdkLoggerPerformanceTest.cpp
+++ unittests/rdkLoggerPerformanceTest.cpp
@@ -401,7 +401,7 @@ TEST_F(RDKLoggerPerformanceTest, DifferentOutputDestinationsTest) {
     
     // Should complete in reasonable time
     EXPECT_LT(elapsed, 10.0) << "Different output destinations test should be reasonably fast";
-    rdk_Log_exit();
+    rdk_log_exit();
 }
 
 // Test logging with format string performance
