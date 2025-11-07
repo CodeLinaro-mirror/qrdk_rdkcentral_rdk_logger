e4052fbea6a434ac523e7af22e3b2d04e65896e9
Deepthi C Shetty <115452109+dshett549@users.noreply.github.com>
2025-11-07 13:05:14 +0530

    Update rdk_debug_priv.c

--- src/rdk_debug_priv.c
+++ src/rdk_debug_priv.c
@@ -57,7 +57,6 @@
 #include <log4c/appender_type_rollingfile.h>
 #include <log4c/rollingpolicy.h>
 #include <log4c/rollingpolicy_type_sizewin.h>
-#include <log4c/rollingpolicy.h>
 
 #ifdef SYSTEMD_JOURNAL
 #include <systemd/sd-journal.h>
