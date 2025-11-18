0deef55e7e9fa57b111e39a44d4a80e454820cb8
Deepthi C Shetty <115452109+dshett549@users.noreply.github.com>
2025-11-18 18:09:25 +0530

    Update test_rdkloggerExtInit.cpp

--- unittests/test_rdkloggerExtInit.cpp
+++ unittests/test_rdkloggerExtInit.cpp
@@ -13,7 +13,7 @@ protected:
     }
     void TearDown() override {
         // Clean shutdown between tests
-        const char* cat_name = "LOG.RDK";
+        /*const char* cat_name = "LOG.RDK";
         log4c_category_t* cat = log4c_category_get(cat_name);
         if (cat) {
             // Get const appender from category
@@ -29,7 +29,7 @@ protected:
                     }
                 }
             }
-        }
+        }*/
         rdk_logger_deinit();
     }
 };
