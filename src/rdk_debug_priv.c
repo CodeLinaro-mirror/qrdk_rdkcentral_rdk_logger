f55fba19bd7267a791555f1db2c5150afc8db016
Deepthi C Shetty <115452109+dshett549@users.noreply.github.com>
2025-11-19 09:42:54 +0530

    Update rdk_debug_priv.c

--- src/rdk_debug_priv.c
+++ src/rdk_debug_priv.c
@@ -548,13 +548,6 @@ void rdk_dbg_priv_log_msg(rdk_LogLevel level, const char *module_name, const cha
             }
         }
 	}
-    if (app) 
-	{
-        if (log4c_appender_open(app) != 0) 
-		{
-            return;
-        }
-    }
 
     if (!IS_LOGGING_ENABLED_FOR_LEVEL(module_name, level))
     {
