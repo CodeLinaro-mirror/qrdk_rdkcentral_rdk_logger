407fbc4907359eaeb3d4c1aaa9d4e17508478ce0
Deepthi C Shetty <115452109+dshett549@users.noreply.github.com>
2025-11-19 16:39:28 +0530

    Update rdkLoggerRotationTest.cpp

--- unittests/rdkLoggerRotationTest.cpp
+++ unittests/rdkLoggerRotationTest.cpp
@@ -105,7 +105,6 @@ protected:
     ASSERT_NE(pid, -1) << "fork failed"; \
     if (pid == 0) { \
         test_body; \
-        rdk_log_exit(); \
         rdk_logger_deinit(); \
         exit(0); \
     } else { \
