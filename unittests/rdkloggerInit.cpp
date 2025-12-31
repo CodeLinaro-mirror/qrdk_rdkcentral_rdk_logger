4894d543c5563b2fe18beada50d6813d8082970a
Deepthi C Shetty <115452109+dshett549@users.noreply.github.com>
2025-12-31 16:12:57 +0530

    Update rdkloggerInit.cpp

--- unittests/rdkloggerInit.cpp
+++ unittests/rdkloggerInit.cpp
@@ -86,13 +86,13 @@ TEST(rdkloggerInit, rdkLogger_ReInit_check_enabled)
   ASSERT_EQ(ret,RDK_SUCCESS)<<"rdk_logger_init failed:"<<ret<<", could not open user configuration file:"<<conf_file;
   ret = rdk_logger_is_logLevel_enabled( "LOG.RDK.ONLYERROR", RDK_LOG_ERROR);
   EXPECT_EQ(ret,TRUE)<<"rdk_logger_is_logLevel_enabled for RDK_LOG_ERROR failed with return:\""<<ret<<"\"";
-
+  ret = rdk_logger_deinit();
   ret = rdk_logger_init(conf_file);
   ASSERT_EQ(ret,RDK_SUCCESS)<<"rdk_logger_init failed:"<<ret<<", could not open user configuration file:"<<conf_file;
   ret = rdk_logger_is_logLevel_enabled( "LOG.RDK.ONLYERROR", RDK_LOG_ERROR);
   EXPECT_EQ(ret,TRUE)<<"rdk_logger_is_logLevel_enabled for RDK_LOG_ERROR failed with return:\""<<ret<<"\"";
 
- // ret = rdk_logger_deinit();
+ ret = rdk_logger_deinit();
  // ASSERT_EQ(ret,RDK_SUCCESS)<<"rdk_logger_deinit failed with return:"<<ret;
 
 }
